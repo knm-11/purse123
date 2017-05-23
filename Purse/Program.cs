1c3f4ea49098e0e351227719ac53687648fd6c55
misha <misha.savchuk@botscrew.com>
2017-05-23 22:18:16 +0300

    first misha commit

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Credit;
 
 namespace Purse
 {
@@ -10,6 +11,8 @@ namespace Purse
     {
         static void Main(string[] args)
         {
+            Class1 c = new Class1();
+            Console.WriteLine(c.czrt);
         }
     }
 }
